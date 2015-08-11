12e07e4f758bf4f72cd1450965586321e4d9da44
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2015-08-12 04:48:35 +0900

    Added abc 006 c and modified abc 006 b

--- abc/006/b.sh
+++ abc/006/b.sh
@@ -1,6 +1,9 @@
-read a;echo "
-len=$a;res[0]=0;res[1]=0;res[2]=0;res[3]=1;
-for(i=4;i<=len;i++){
-  res[i%4]=(res[(i-1)%4]+res[(i-2)%4]+res[(i-3)%4])%10007;
+awk '
+{
+  len=$0;res[0]=0;res[1]=0;res[2]=0;res[3]=1;
+  for(i=4;i<=len;i++){
+    res[i%4]=(res[(i-1)%4]+res[(i-2)%4]+res[(i-3)%4])%10007;
+  }
+  print res[len%4]
 }
-res[len%4]"|bc
+'
